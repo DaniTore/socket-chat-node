b2cc5c1f6ab216923afbee33f8d816785bc434eb
Daniel Tore <danieltore@gmail.com>
2021-01-08 17:27:30 +0100

    delete user ready

--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -1,42 +1,41 @@
 const { io } = require('../server');
 
+const { Usuarios } = require('../classes/usuarios');
+
+const usuarios = new Usuarios();
 
 io.on('connection', (client) => {
 
-    console.log('Usuario conectado');
+    // escucha el emit del front 'entradaChat'
+    client.on('entrarChat', (usuario, callback) => {
 
-    client.emit('enviarMensaje', {
-        usuario: 'Administrador',
-        mensaje: 'Bienvenido a esta aplicación'
-    });
+        if (!usuario.nombre) {
+            return callback({
+                error: true,
+                mensaje: 'El nombre es necesario'
+            });
+        }
 
+        let personasConectadas = usuarios.agregarPersonaAlChat(client.id, usuario.nombre);
 
+        // evento hacía front comunica todas las personas conectadas al chat
+        client.broadcast.emit('listaPersonasConectadas', usuarios.getTodasLasPersonas());
+
+        callback(personasConectadas)
+    })
 
     client.on('disconnect', () => {
-        console.log('Usuario desconectado');
-    });
 
-    // Escuchar el cliente
-    client.on('enviarMensaje', (data, callback) => {
+        // devuelve la persona borrada
+        let personaborrada = usuarios.borrarPersonaEnChat(client.id);
 
-        console.log(data);
+        client.broadcast.emit('abandonaChat', {
+            usuario: 'Administrador',
+            mensaje: `${personaborrada.nombre} abandonó el chat`
+        });
 
-        client.broadcast.emit('enviarMensaje', data);
+        client.broadcast.emit('listaPersonasConectadas', usuarios.getTodasLasPersonas());
 
-
-        // if (mensaje.usuario) {
-        //     callback({
-        //         resp: 'TODO SALIO BIEN!'
-        //     });
-
-        // } else {
-        //     callback({
-        //         resp: 'TODO SALIO MAL!!!!!!!!'
-        //     });
-        // }
-
-
-
-    });
+    })
 
 });
